be6ecda3a51ae892793404a9998656487018b687
Alexis Correa <alexis.correa026@gmail.com>
2022-05-10 19:45:51 -0300

    Nueva ruta: Ruta post de usuarios creada

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,12 +4,12 @@ var router = express.Router();
 let getProducts = require('../controllers/Products/getProducts.js')
 let postProduct = require('../controllers/Products/postProduct.js')
 let putProduct = require('../controllers/Products/putProduct.js')
-let deleteProduct = require('../controllers/Products/deleteProduct.js')
+// let deleteProduct = require('../controllers/Products/deleteProduct.js')
 
 router.get('/:id', getProducts);
 router.get('/', getProducts);
 router.post('/', postProduct);
 router.put('/:id', putProduct);
-router.delete('/:id', deleteProduct);
+// router.delete('/:id', deleteProduct);
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,13 +2,13 @@ var express = require("express");
 var router = express.Router();
 
 const postUser = require("../controllers/Users/postUser");
-const getUser = require("../controllers/Users/getUser");
-const deleteUser = require("../controllers/Users/deleteUser");
-const putUser = require("../controllers/Users/putUser");
+//const getUser = require("../controllers/Users/getUser");
+//const deleteUser = require("../controllers/Users/deleteUser");
+//const putUser = require("../controllers/Users/putUser");
 
 router.post("/", postUser);
-router.get("/", getUser);
-router.delete("/", deleteUser);
-router.put("/", putUser);
+//router.get("/", getUser);
+//router.delete("/", deleteUser);
+//router.put("/", putUser);
 
 module.exports = router;
