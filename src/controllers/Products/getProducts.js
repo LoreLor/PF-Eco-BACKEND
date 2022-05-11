b4cf907f7345950917f79ba76879e47ef7414a59
Ramira10 <matiasramira11@hotmail.com>
2022-05-11 15:22:41 -0300

    Fix getProducts, and add getProductByID

--- src/controllers/Products/getProducts.js
+++ src/controllers/Products/getProducts.js
@@ -1,22 +1,30 @@
-const getById = require("../../services/Products/productsID");
-const getCategory = require("../../services/Products/productCategory");
-const allProducts = require("../../services/Products/allProducts");
+const Category = require("../../models/Category.js");
+const Product = require("../../models/Product.js");
+const normalizeString = require("../../utils/normalizeString.js");
 
-async function getProducts(req, res) {
+const getProducts = async(req, res, next)=>{
+  const { name } = req.query;
   try {
-    const { category } = req.query;
-    const { id } = req.params;
-
-    if (id) {
-      await getById(id, req, res);
-    }
-    if (category) {
-      await getCategory(category, req, res);
-    } else {
-      await allProducts(res);
-    }
-  } catch (err) {
-    console.log(err);
+      let allProducts = await Product.findAll({
+          include: [{
+            model: Category,
+            attributes: ["name"],
+            through: {
+              attributes: [],
+            }
+          }]
+        })
+      if (name) {
+          let productByName = allProducts.filter(r => normalizeString(r.dataValues.name).includes(normalizeString(name.toString())));
+          if(productByName.length > 0) {
+              return res.status(200).send(productByName);
+          }
+          return res.status(400).send("Producto no encontrado.");
+      } else {
+          res.status(200).send(allProducts);
+      }
+  } catch (error) {
+      next(error);
   }
 }
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -4,10 +4,11 @@ var router = express.Router();
 let getProducts = require('../controllers/Products/getProducts')
 let postProduct = require('../controllers/Products/postProduct.js')
 let putProduct = require('../controllers/Products/putProduct.js')
-let deleteProduct = require('../controllers/Products/deleteProduct.js')
+let deleteProduct = require('../controllers/Products/deleteProduct.js');
+const getProductByID = require("../controllers/Products/getProductByID");
 
-router.get('/:id', getProducts);
 router.get('/', getProducts);
+router.get('/:id', getProductByID);
 router.post('/', postProduct);
 router.put('/:id', putProduct);
 router.delete('/:id', deleteProduct);
