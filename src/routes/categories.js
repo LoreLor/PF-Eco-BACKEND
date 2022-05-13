9025e1e6b2196265c4cdcadb68e3b02ec17a9aea
Ramira10 <matiasramira11@hotmail.com>
2022-05-13 14:38:12 -0300

    fixes

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -3,14 +3,12 @@ var router = express.Router();
 const getCategories = require("../controllers/Categories/getCategories.js");
 const getCategory = require('../controllers/Categories/getCategory.js')
 const postCategory = require("../controllers/Categories/postCategory.js");
-// const bulkCategories = require("../controllers/Categories/bulkCategories.js");
 const deleteCategory = require("../controllers/Categories/deleteCategory.js");
 const putCategory = require("../controllers/Categories/putCategory.js");
 
 router.get('/', getCategories)
 router.get('/:name', getCategory)
 router.post('/', postCategory)
-// router.post('/bulk', bulkCategories)
 router.delete('/:id', deleteCategory)
 router.put("/:id", putCategory)
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 let getProducts = require('../controllers/Products/getProducts.js')
 let postProduct = require('../controllers/Products/postProduct.js')
-// let bulkProducts = require('../controllers/Products/bulkProducts.js')
 let putProduct = require('../controllers/Products/putProduct.js')
 let deleteProduct = require('../controllers/Products/deleteProduct.js');
 const getProductByID = require("../controllers/Products/getProductByID");
@@ -11,7 +10,6 @@ const getProductByID = require("../controllers/Products/getProductByID");
 router.get('/', getProducts);
 router.get('/:id', getProductByID);
 router.post('/', postProduct);
-// router.post('/bulk', bulkProducts);
 router.put('/:id', putProduct);
 router.delete('/:id', deleteProduct);
 
