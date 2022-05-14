a7b5a9b056dd258cc917ef8fbc7045d232382244
SebastianUrquijo <sebastian.urquijo98@gmail.com>
2022-05-13 22:11:09 -0500

    prevalidations

--- src/controllers/Products/testImage.js
+++ src/controllers/Products/testImage.js
@@ -1,8 +1,37 @@
-const testInput =(req,res,next)=>{
-   console.log(req.files)
-   console.log(req.body.name)
-   res.send("uploaded")
-   
+const cloudinary = require('../../middleware/cloudinary.js')
+const fs = require('fs')
+
+const cloudinaryMethod = async (file) =>{
+    return new Promise (resolve=>{
+        cloudinary.uploader.upload(file,(err,res)=>{
+            if(err) return res.status(500).send("Upload fail")
+            console.log(res.secure_url)
+            resolve({
+                res: res. secure_url
+            })
+        })
+    })
+}
+
+const testInput =async (req,res,next)=>{
+    const {input} = req.body
+    const formData = JSON.parse(input)
+    console.log(formData)
+    console.log(req.files)
+    
+    try {
+    const urls = [];
+    const files = req.files;
+    for(const file of files){
+        const {path} = file;
+        const newPath = await cloudinaryMethod(path)
+        urls.push(newPath)
+        fs.unlinkSync(path)
+    }
+    res.send({urls,formData})
+  } catch (error) {
+      console.log(error)
+  }
 }
 
 module.exports = testInput;
