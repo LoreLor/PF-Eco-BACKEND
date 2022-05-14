5de8fae9f471f704bcaf237308305f1f292e79ce
SebastianUrquijo <sebastian.urquijo98@gmail.com>
2022-05-14 12:19:32 -0500

    lolas

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,11 @@ var router = express.Router();
 let routerProducts = require("./products.js");
 let routerCategories = require("./categories.js");
 let routerUsers = require("./users.js");
-let testData = require("./test.js")
+
 
 router.use("/products", routerProducts)
 router.use("/categories", routerCategories)
 router.use("/user", routerUsers)
-router.use("/test",testData)
+
 
 module.exports = router;
--- src/routes/test.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const uploadPictures = require('../middleware/multer.js')
-
-let testInput = require ('../controllers/Products/testImage.js')
-
-router.post('/',uploadPictures(), testInput)
-
-module.exports = router;
