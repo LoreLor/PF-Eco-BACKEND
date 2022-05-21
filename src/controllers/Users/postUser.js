d7acfd5de5fb358c7d350bc77a51fc76814d7733
SebastianUrquijo <sebastian.urquijo98@gmail.com>
2022-05-21 13:48:13 -0500

    RegisterRoute

--- src/controllers/Users/postUser.js
+++ src/controllers/Users/postUser.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt")
 const normalizeString = require('../../utils/normalizeString.js');
 
 
-const postUser = async (req, res, next) => {
+/* const postUser = async (req, res, next) => {
     const { name, last_name, user_name, email, phone_number, password, dni, address, birthday} = req.body;
     if (!name && !last_name && !user_name && !email && !password && !address) {
         res.status(400).json({ msg: "Faltan datos" });
@@ -35,6 +35,35 @@ const postUser = async (req, res, next) => {
             birthday: user.birthday,
             isActive: user.isActive
         });
+    } catch (error) {
+        next(error);
+    }
+} */
+
+const postUser = async (req, res, next) => {
+    const { name, last_name, user_name, email,password} = req.body;
+    if (!name && !last_name && !user_name && !email && !password) {
+        res.status(400).json({ msg: "Missing data" });
+    }
+    try {
+
+        const [existUN,existEmail] = await Promise.all([
+            User.findOne({where:{user_name:user_name}}),
+            User.findOne({where:{email:email}}),
+        ])
+        if(existUN || existEmail){
+            return res.status(200).json({msg: "User or Email already registered"})
+        }
+        const userNew = User.create({
+            name,
+            last_name,
+            user_name,
+            email: email,
+            password: bcrypt.hashSync(password, 8),
+        })
+        if(userNew){
+            return res.status(201).json({msg:"User registered"})
+        }
     } catch (error) {
         next(error);
     }
