1197d9ec2578449977b09af737da4727bba053b9
Ramira10 <matiasramira11@hotmail.com>
2022-05-24 14:08:46 -0300

    fixes

--- src/controllers/Review/getReviewsOfProduct.js
+++ src/controllers/Review/getReviewsOfProduct.js
@@ -16,37 +16,47 @@ const getReviewsOfProduct = async (req, res, next) => {
             }]
         })
 
-        var sum = 0
+        if (product.dataValues.details)
+
+            var sum = 0
         var cont = 0
         var prom = 0
+
         for (let i = 0; i < product.dataValues.details.length; i++) {
-            sum = sum + product.dataValues.details[i].dataValues.review.dataValues.points
-            cont++;
+            if (product.dataValues.details[i].dataValues.review !== null) {
+                sum = sum + product.dataValues.details[i].dataValues.review.dataValues.points
+                cont++;
+            }
+
         }
 
-       prom = Math.ceil(sum/cont)
-
-       await Product.update(
-        {
-          rating: prom
-        },
-        {
-          where: {
-            id: productId
-          },
+        if (sum && cont) {
+            prom = Math.ceil(sum / cont)
         }
-        )
 
-        if(product) {
+        if (prom) {
+            await Product.update(
+                {
+                    rating: prom
+                },
+                {
+                    where: {
+                        id: productId
+                    },
+                }
+            )
+        }
+
+        if (product) {
             var detalles = product.dataValues.details;
-            if(detalles.length) {
+            if (detalles.length) {
                 var reviews = [];
                 for (let i = 0; i < detalles.length; i++) {
-                    if(detalles[i].review) {
+                    if (detalles[i].review) {
                         reviews.push(detalles[i].review)
                     }
                 }
-                if(reviews.length) {
+                if (reviews.length) {
                     return res.status(200).send(reviews);
                 } else {
                     return res.status(200).send("This product has no reviews.")
