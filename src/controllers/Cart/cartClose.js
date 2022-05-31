24d4039bbba557ab3b8106f73be7dff22d843e38
Alexis Correa <alexis.correa026@gmail.com>
2022-05-31 00:08:01 -0300

    cartClose
    Creacion de la ruta para cerrar el carrito del usuario, asignarle uno nuevo y a su vez actualizar stock de los productos comprados

--- src/controllers/Cart/cartClose.js
+++ src/controllers/Cart/cartClose.js
@@ -1,9 +1,76 @@
+const Product = require("../../models/Product.js");
+const Cart = require("../../models/Cart.js");
+const User = require("../../models/User.js");
+const Detail = require("../../models/Detail.js");
 
-
-//Obtenemos el userId
-//Obtenemos el cartId
 //Actualizamos ese cart a -> status: pagado, fecha: fecha_actual, open: false
 
 //Actualizar los stock de los productos que ya fueron comprados
 //  Restar los bundles de los productos al stock principal del producto en cuestion
 
+const cartClose = async (req, res, next) => {
+  try {
+    const { userId, cartId } = req.params;
+
+    const allData = await Cart.findOne({
+      where: {
+        id: cartId,
+        userId: userId,
+      },
+      include: {
+        model: Detail,
+        include: {
+          model: Product,
+        },
+      },
+    });
+
+    //Actualizar y cerrar el carrito del usuario
+    await Cart.update(
+      {
+        status: "paid",
+        date: new Date(),
+        open: false,
+        price_total: allData.price_total,
+      },
+      {
+        where: {
+          id: cartId,
+          userId: userId,
+        },
+      }
+    );
+
+    allData.details.forEach(async (details) => {
+      await Product.update(
+        {
+          stock:
+            parseInt(allData.details[0].product.stock, 10) -
+            parseInt(details.bundle, 10),
+        },
+        {
+          where: {
+            id: details.productId,
+          },
+        }
+      );
+    });
+
+    //Crear un nuevo carrito al usuario
+    await Cart.create({
+      payment_method: null,
+      date: null,
+      status: null,
+      open: true,
+      userId: userId,
+      price_total: 0,
+    });
+
+    res.status(200).send("Carro cerrado y stcok actualizado");
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports = cartClose;
