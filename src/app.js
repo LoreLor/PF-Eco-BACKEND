02b1e2b7e9e5287942ade7ec4f4848a4b08dec12
Lorena De Armas <lorenadearmas01@gmail.com>
2023-01-31 18:04:26 -0300

    environment

--- src/app.js
+++ src/app.js
@@ -25,8 +25,8 @@ app.set('view engine', 'ejs');
 
 
 app.use((req,res,next)=>{
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', 'https://pf-eco-frontend-lorelor.vercel.app/') //'http://localhost:3000')
+  res.header('Access-Control-Allow-Credentials', 'true')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
