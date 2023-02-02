43f5e1f27927a215e11e57fbdbd0b78eb8b64a02
Lorena De Armas <lorenadearmas01@gmail.com>
2023-02-02 14:25:49 -0300

    config-db

--- src/db.js
+++ src/db.js
@@ -3,25 +3,23 @@ require('dotenv').config();
 //Instancia de la base de datos - Sequelize
 const {Sequelize} = require('sequelize');
 
-// const {
-//     DB_HOST,
-//     DB_USER,
-//     DB_PASS,
-//     DB_NAME,
-//     PORT
-// } = process.env;
+const {
+    DB_HOST,
+    DB_USER,
+    DB_PASS,
+    DB_NAME,
+    PORT
+} = process.env;
 
-// //Conexion a la base de datos
-// const db = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${PORT}/${DB_NAME}`, {
-//     logging: false,
+//Conexion a la base de datos
+const db = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${PORT}/${DB_NAME}`, {
+    logging: false,
+    native: false,
+    define: {
+    timestamps: false
+  }
 
-//     native: false,
-//     define: {
-//     timestamps: false
-//   }
-
-//});
-const db = new Sequelize(process.env.DATABASE_URL)
+});
 
 
 module.exports = db
